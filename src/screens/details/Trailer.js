cbcb08174bbdcf557e69efa3c21d3534f8eeb131
Krishna Kumar Singh <03kksingh@gmail.com>
2022-08-19 18:01:45 +0530

    fifth commit

---
+++ src/screens/details/Trailer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import YouTube from 'react-youtube';
+
+class Trailer extends React.Component {
+  render() {
+    const opts = {
+      height: '390',
+      width: '100%',
+      playerVars: {
+        // https://developers.google.com/youtube/player_parameters
+        autoplay: 1,
+      },
+    };
+
+    return <YouTube videoId={this.props.id} opts={opts} onReady={this._onReady} />;
+  }
+
+  _onReady(event) {
+    // access to player in all event handlers via event.target
+    event.target.pauseVideo();
+  }
+}
+export default Trailer;
